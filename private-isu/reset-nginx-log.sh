8e9d2f125e2d83509c5ab625b763766452bc5e53
sunakan <sunakan03@gmail.com>
2024-08-24 13:16:38 +0900

    [private-isu] nginxのログをリセットするスクリプトのコメント出力を整理

--- private-isu/reset-nginx-log.sh
+++ private-isu/reset-nginx-log.sh
@@ -3,21 +3,26 @@
 set -eu
 
 echo '----[ BEFORE ]'
-echo '行数: /var/log/nginx/access.log'
+echo -n '/var/log/nginx/access.log の行数:'
 cat /var/log/nginx/access.log | wc -l
-echo '行数: /var/log/nginx/error.log'
+echo -n '/var/log/nginx/error.log の行数:'
 cat /var/log/nginx/error.log | wc -l
+echo ''
 
-echo '----[ RESET ]'
+echo '----[ LOG BACKUP ]'
 mv /var/log/nginx/access.log /var/log/nginx/access.log.old
-echo 'DONE: mv /var/log/nginx/access.log /var/log/nginx/access.log.old'
+echo '/var/log/nginx/access.log.oldにバックアップを取りました'
 mv /var/log/nginx/error.log /var/log/nginx/error.log.old
-echo 'DONE: mv /var/log/nginx/error.log /var/log/nginx/error.log.old'
+echo '/var/log/nginx/error.log.oldにバックアップを取りました'
+echo ''
+
+echo '----[ RESTART ]'
 systemctl reload nginx
-echo 'DONE: systemctl reload nginx'
+echo 'Nginxを再起動しました'
+echo ''
 
 echo '----[ AFTER ]'
-echo '行数: /var/log/nginx/access.log'
+echo -n '/var/log/nginx/access.log の行数:'
 cat /var/log/nginx/access.log | wc -l
-echo '行数: /var/log/nginx/error.log'
+echo -n '/var/log/nginx/error.log の行数:'
 cat /var/log/nginx/error.log | wc -l
