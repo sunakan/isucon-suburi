2e098226a89456b6480cd711dc302afcebb9f1bf
sunakan <sunakan03@gmail.com>
2024-08-24 13:16:21 +0900

    [private-isu] mysqlのスロークエリログのリセットスクリプトで出力するコメントを修正

--- private-isu/reset-mysql-slow-query-log.sh
+++ private-isu/reset-mysql-slow-query-log.sh
@@ -14,7 +14,8 @@ sudo -u mysql mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.old
 echo '/var/log/mysql/mysql-slow.log.oldにバックアップを取りました'
 echo ''
 
-echo '----[ AFTER ]'
+
+echo '----[ RESTART ]'
 # slow_query_logとlong_query_timeがある行を削除
 sudo sed -i '/slow_query/d' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo sed -i '/long_query_time/d' /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -24,10 +25,11 @@ sudo sed -i '/long_query_time/d' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo echo 'slow_query_log      = 1' >> /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo echo 'slow_query_log_file = /var/log/mysql/mysql-slow.log' >> /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo echo 'long_query_time     = 0' >> /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo tail -n 5 /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo tail -n 3 /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo systemctl restart mysql
-echo 'MySQLを再起動しました'
-
+echo 'MySQLを再起動しました(スロークエリも有効化)'
 echo ''
+
+echo '----[ AFTER ]'
 echo -n '/var/log/mysql/mysql-slow.log の行数:'
 sudo -u mysql cat /var/log/mysql/mysql-slow.log | wc -l
