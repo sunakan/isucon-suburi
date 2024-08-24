963cfdddf8433c483dc720de1e18f9c44d0fe93e
sunakan <sunakan03@gmail.com>
2024-08-24 11:34:30 +0900

    [private-isu] reset-nginx-logでmv先のパスを修正(var/* → /var/*)

--- private-isu/reset-nginx-log.sh
+++ private-isu/reset-nginx-log.sh
@@ -9,10 +9,10 @@ echo '行数: /var/log/nginx/error.log'
 cat /var/log/nginx/error.log | wc -l
 
 echo '----[ RESET ]'
-mv /var/log/nginx/access.log var/log/nginx/access.log.old
-echo 'DONE: mv /var/log/nginx/access.log var/log/nginx/access.log.old'
-mv /var/log/nginx/error.log var/log/nginx/error.log.old
-echo 'DONE: mv /var/log/nginx/error.log var/log/nginx/error.log.old'
+mv /var/log/nginx/access.log /var/log/nginx/access.log.old
+echo 'DONE: mv /var/log/nginx/access.log /var/log/nginx/access.log.old'
+mv /var/log/nginx/error.log /var/log/nginx/error.log.old
+echo 'DONE: mv /var/log/nginx/error.log /var/log/nginx/error.log.old'
 systemctl reload nginx
 echo 'DONE: systemctl reload nginx'
 
