e4824b7b6279b5e1aa1ad98d6ca84ecdd0319ec3
sunakan <sunakan03@gmail.com>
2024-08-24 11:23:01 +0900

    [private-isu] reset-nginx-logの最後に、ログファイルの行数を表示する

--- private-isu/reset-nginx-log.sh
+++ private-isu/reset-nginx-log.sh
@@ -2,3 +2,8 @@
 
 rm -rf /var/log/nginx/*.log && echo '/var/log/nginx/*.logを削除しました'
 systemctl reload nginx && echo 'nginxをreloadしました'
+
+echo '/var/log/nginx/access.logの行数'
+cat /var/log/nginx/access.log | wc -l
+echo '/var/log/nginx/error.logの行数'
+cat /var/log/nginx/error.log | wc -l
