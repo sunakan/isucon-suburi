fe07bcc844a352aa965fb30a0207706ba6070c8d
sunakan <sunakan03@gmail.com>
2024-08-25 10:11:16 +0900

    [private-isu] typo comment

--- private-isu/02-increase-worker-process.sh
+++ private-isu/02-increase-worker-process.sh
@@ -4,7 +4,7 @@ set -eu
 
 echo "----[ 上書き ]"
 echo "FROM: /tmp/isucon-suburi/private-isu/files/02-unicorn_config.rb"
-echo "TO: /home/isucon/private_isu/webapp/ruby/uncorn_config.rb"
+echo "TO: /home/isucon/private_isu/webapp/ruby/unicorn_config.rb"
 echo "----"
 
 sudo -u isucon cp /tmp/isucon-suburi/private-isu/files/02-unicorn_config.rb /home/isucon/private_isu/webapp/ruby/unicorn_config.rb
