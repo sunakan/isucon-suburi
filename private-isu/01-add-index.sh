2316cb2229018807f8ba46ddbffeec1b078e4ea3
sunakan <sunakan03@gmail.com>
2024-08-25 09:55:34 +0900

    [private-isu] インデックス追加にechoでコメントを表示させる

--- private-isu/01-add-index.sh
+++ private-isu/01-add-index.sh
@@ -2,10 +2,11 @@
 
 set -eu
 
-# テーブルにインデックスを貼る
-#
-# 対象DB: isuconp
-# 対象テーブル: comments
-# インデックス名: post_id_idx
-# カラム: post_id, created_at
+echo "----[ インデックス追加 ]"
+echo "対象DB: isuconp"
+echo "対象テーブル: comments"
+echo "インデックス名: post_id_idx"
+echo "カラム: post_id, created_at"
+echo "----"
+
 sudo mysql isuconp -e "alter table comments add index post_id_idx (post_id, created_at desc)"
