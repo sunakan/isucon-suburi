e6d29a25653dd15e0d6dd4d003d8d0c588dc4db6
sunakan <sunakan03@gmail.com>
2024-08-25 10:08:15 +0900

    [private-isu] worker processを増やす設定に書き換えた後、再起動させる

--- private-isu/02-increase-worker-process.sh
+++ private-isu/02-increase-worker-process.sh
@@ -8,3 +8,6 @@ echo "TO: /home/isucon/private_isu/webapp/ruby/uncorn_config.rb"
 echo "----"
 
 sudo -u isucon cp /tmp/isucon-suburi/private-isu/files/02-unicorn_config.rb /home/isucon/private_isu/webapp/ruby/uncorn_config.rb
+
+sudo systemctl restart isu-ruby
+echo "isu-rubyを再起動しました"
