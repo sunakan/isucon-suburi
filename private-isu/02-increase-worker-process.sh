b4f3d3d1c01c7a9dca243ef3e29f5493450121b4
sunakan <sunakan03@gmail.com>
2024-08-25 09:55:02 +0900

    [private-isu] unicorn_config.rbの上書きscriptを用意

---
+++ private-isu/02-increase-worker-process.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eu
+
+echo "----[ 上書き ]"
+echo "FROM: /tmp/isucon-suburi/private-isu/files/02-unicorn_config.rb"
+echo "TO: /home/isucon/private_isu/webapp/ruby/uncorn_config.rb"
+echo "----"
+
+mv /tmp/isucon-suburi/private-isu/files/02-unicorn_config.rb /home/isucon/private_isu/webapp/ruby/uncorn_config.rb
