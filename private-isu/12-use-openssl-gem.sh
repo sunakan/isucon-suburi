1176fa0511c200ddeff78c80face5029175ac674
sunakan <sunakan03@gmail.com>
2024-09-01 11:41:27 +0900

    [private-isu] 12のshを記述

---
+++ private-isu/12-use-openssl-gem.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+echo "----[ app.rbの上書き ]"
+echo "FROM: /tmp/isucon-suburi/private-isu/files/12-app.rb"
+echo "TO: /home/isucon/private_isu/webapp/ruby/app.rb"
+echo "----"
+
+sudo -u isucon cp /tmp/isucon-suburi/private-isu/files/12-app.rb /home/isucon/private_isu/webapp/ruby/app.rb
+
+sudo systemctl restart isu-ruby
+echo "isu-rubyを再起動しました"
