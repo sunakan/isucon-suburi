11b0a7df17d0c01fb72fbfcc67e40c2c5f1566fd
sunakan <sunakan03@gmail.com>
2024-09-07 12:32:33 +0900

    newrelicをisu-rubyに導入(newrelic.yml以外)

---
+++ private-isu/14-add-newrelic.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eu
+
+echo "----[ Gemfilの上書き ]"
+echo "FROM: /tmp/isucon-suburi/private-isu/files/14-Gemfile"
+echo "TO: /home/isucon/private_isu/webapp/ruby/Gemfile"
+echo "----"
+
+sudo -u isucon cp /tmp/isucon-suburi/private-isu/files/14-Gemfile /home/isucon/private_isu/webapp/ruby/Gemfile
+
+echo "----[ app.rbの上書き ]"
+echo "FROM: /tmp/isucon-suburi/private-isu/files/14-app.rb"
+echo "TO: /home/isucon/private_isu/webapp/ruby/app.rb"
+echo "----"
+
+sudo -u isucon cp /tmp/isucon-suburi/private-isu/files/14-app.rb /home/isucon/private_isu/webapp/ruby/app.rb
+
+echo "----[ bundle install ]"
+
+sudo su - isucon
+cd /home/isucon/private_isu/webapp/ruby/
+export PATH=${PATH}:/home/isucon/.local/ruby/bin/
+/home/isucon/.local/ruby/bin/bundle install
+
+sudo systemctl restart isu-ruby
+echo "isu-rubyを再起動しました"
